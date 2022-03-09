e2d0faaf50c7a7a778ebf44ff46d657d9ea4eca9
Daniyar <Daniyar@ANCIENT>
2022-03-10 03:43:24 +0600

    Добавьте файлы проекта.

---
+++ Task 3/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Task_3
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridViewCustomers = new System.Windows.Forms.DataGridView();
+            this.labelProcessor1 = new System.Windows.Forms.Label();
+            this.labelProcessor2 = new System.Windows.Forms.Label();
+            this.labelDenied = new System.Windows.Forms.Label();
+            this.dataGridViewProcessor1 = new System.Windows.Forms.DataGridView();
+            this.dataGridViewProcessor2 = new System.Windows.Forms.DataGridView();
+            this.updater = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCustomers)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewProcessor1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewProcessor2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewCustomers
+            // 
+            this.dataGridViewCustomers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewCustomers.Location = new System.Drawing.Point(12, 12);
+            this.dataGridViewCustomers.Name = "dataGridViewCustomers";
+            this.dataGridViewCustomers.RowHeadersWidth = 51;
+            this.dataGridViewCustomers.RowTemplate.Height = 24;
+            this.dataGridViewCustomers.Size = new System.Drawing.Size(758, 74);
+            this.dataGridViewCustomers.TabIndex = 0;
+            // 
+            // labelProcessor1
+            // 
+            this.labelProcessor1.AutoSize = true;
+            this.labelProcessor1.Font = new System.Drawing.Font("Consolas", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelProcessor1.Location = new System.Drawing.Point(56, 93);
+            this.labelProcessor1.Name = "labelProcessor1";
+            this.labelProcessor1.Size = new System.Drawing.Size(168, 27);
+            this.labelProcessor1.TabIndex = 1;
+            this.labelProcessor1.Text = "Processor1: ";
+            // 
+            // labelProcessor2
+            // 
+            this.labelProcessor2.AutoSize = true;
+            this.labelProcessor2.Font = new System.Drawing.Font("Consolas", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelProcessor2.Location = new System.Drawing.Point(580, 93);
+            this.labelProcessor2.Name = "labelProcessor2";
+            this.labelProcessor2.Size = new System.Drawing.Size(168, 27);
+            this.labelProcessor2.TabIndex = 2;
+            this.labelProcessor2.Text = "Processor2: ";
+            // 
+            // labelDenied
+            // 
+            this.labelDenied.AutoSize = true;
+            this.labelDenied.Font = new System.Drawing.Font("Consolas", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelDenied.Location = new System.Drawing.Point(336, 93);
+            this.labelDenied.Name = "labelDenied";
+            this.labelDenied.Size = new System.Drawing.Size(116, 27);
+            this.labelDenied.TabIndex = 3;
+            this.labelDenied.Text = "Denied: ";
+            // 
+            // dataGridViewProcessor1
+            // 
+            this.dataGridViewProcessor1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewProcessor1.Location = new System.Drawing.Point(12, 134);
+            this.dataGridViewProcessor1.Name = "dataGridViewProcessor1";
+            this.dataGridViewProcessor1.RowHeadersWidth = 51;
+            this.dataGridViewProcessor1.RowTemplate.Height = 24;
+            this.dataGridViewProcessor1.Size = new System.Drawing.Size(370, 74);
+            this.dataGridViewProcessor1.TabIndex = 4;
+            // 
+            // dataGridViewProcessor2
+            // 
+            this.dataGridViewProcessor2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewProcessor2.Location = new System.Drawing.Point(400, 134);
+            this.dataGridViewProcessor2.Name = "dataGridViewProcessor2";
+            this.dataGridViewProcessor2.RowHeadersWidth = 51;
+            this.dataGridViewProcessor2.RowTemplate.Height = 24;
+            this.dataGridViewProcessor2.Size = new System.Drawing.Size(370, 74);
+            this.dataGridViewProcessor2.TabIndex = 5;
+            // 
+            // updater
+            // 
+            this.updater.Interval = 1;
+            this.updater.Tick += new System.EventHandler(this.updater_Tick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.ClientSize = new System.Drawing.Size(782, 219);
+            this.Controls.Add(this.dataGridViewProcessor2);
+            this.Controls.Add(this.dataGridViewProcessor1);
+            this.Controls.Add(this.labelDenied);
+            this.Controls.Add(this.labelProcessor2);
+            this.Controls.Add(this.labelProcessor1);
+            this.Controls.Add(this.dataGridViewCustomers);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Task 3";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCustomers)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewProcessor1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewProcessor2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewCustomers;
+        private System.Windows.Forms.Label labelProcessor1;
+        private System.Windows.Forms.Label labelProcessor2;
+        private System.Windows.Forms.Label labelDenied;
+        private System.Windows.Forms.DataGridView dataGridViewProcessor1;
+        private System.Windows.Forms.DataGridView dataGridViewProcessor2;
+        private System.Windows.Forms.Timer updater;
+    }
+}
+
---
+++ Task 3/Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task_3
+{
+    public partial class MainForm : Form
+    {
+        private static object _locker = new object();
+
+        Queue<int> queue = new Queue<int>();
+        int customers;
+        int amount1;
+        int amount2;
+        int denies;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            for (int i = 1; i <= 100; ++i)
+            {
+                dataGridViewCustomers.Columns.Add(Convert.ToString(i), Convert.ToString(i));
+                dataGridViewCustomers.Columns[i - 1].Width = 20;
+            }
+                
+
+            for (int i = 1; i <= 100; ++i)
+            {
+                dataGridViewProcessor1.Columns.Add(Convert.ToString(i), Convert.ToString(i));
+                dataGridViewProcessor1.Columns[i - 1].Width = 20;
+            }
+                
+
+            for (int i = 1; i <= 100; ++i)
+            {
+                dataGridViewProcessor2.Columns.Add(Convert.ToString(i), Convert.ToString(i));
+                dataGridViewProcessor2.Columns[i - 1].Width = 20;
+            }
+                
+
+
+            System.Threading.Thread queueGenerator = new System.Threading.Thread(QueueGenerator);
+            System.Threading.Thread processor1 = new System.Threading.Thread(Processor1);
+            System.Threading.Thread processor2 = new System.Threading.Thread(Processor2);
+
+            queueGenerator.Start();
+            processor1.Start();
+            processor2.Start();
+
+            updater.Start();
+        }
+
+        private void QueueGenerator()
+        {
+            int queueSize = 10;
+
+            Random random = new Random();
+
+            for (customers = 1; customers <= 100; ++customers)
+            {
+                int temp = random.Next(1, 100);
+
+                dataGridViewCustomers.Rows[0].Cells[customers - 1].Value = temp;
+
+                if (queue.Count != 10)
+                {
+                    queue.Enqueue(temp);
+                    dataGridViewCustomers.Rows[0].Cells[customers - 1].Style.BackColor = Color.Green;
+                }
+                else
+                {
+                    denies++;
+                    dataGridViewCustomers.Rows[0].Cells[customers - 1].Style.BackColor = Color.Red;
+                }
+                    
+                System.Threading.Thread.Sleep(1000);
+            }
+
+            customers--;
+        }
+
+        private void Processor1()
+        {
+            Queue<int> priorityQueue = new Queue<int>();
+            Queue<int> ordinaryQueue = new Queue<int>();
+
+            while (true)
+            {
+                System.Threading.Thread.Sleep(2000);
+
+                lock (_locker)
+                {
+                    if (queue.Count != 0)
+                    {
+                        if (queue.Peek() % 2 == 0)
+                        {
+                            priorityQueue.Enqueue(queue.Peek());
+                            queue.Dequeue();
+                        }
+                        else
+                        {
+                            ordinaryQueue.Enqueue(queue.Peek());
+                            queue.Dequeue();
+                        }
+                    }
+                }
+                
+                    
+                if (priorityQueue.Count != 0)
+                {
+                    dataGridViewProcessor1.Rows[0].Cells[amount1].Value = priorityQueue.Peek();
+                    dataGridViewProcessor1.Rows[0].Cells[amount1].Style.BackColor = Color.Yellow;
+                    priorityQueue.Dequeue();
+                    amount1++;
+                    continue;
+                }
+
+                if (ordinaryQueue.Count != 0)
+                {
+                    dataGridViewProcessor1.Rows[0].Cells[amount1].Value = ordinaryQueue.Peek();
+                    dataGridViewProcessor1.Rows[0].Cells[amount1].Style.BackColor = Color.Brown;
+                    ordinaryQueue.Dequeue();
+                    amount1++;
+                    continue;
+                }
+
+                if (queue.Count == 0 &&
+                    customers == 100)
+                {
+                    //labelProcessor1.Text = "Processor1: " + Convert.ToString(amount);
+                    break;
+                }
+            }
+        }
+
+        private void Processor2()
+        {
+            Queue<int> priorityQueue = new Queue<int>();
+            Queue<int> ordinaryQueue = new Queue<int>();
+
+            while (true)
+            {
+                System.Threading.Thread.Sleep(2000);
+
+                lock (_locker)
+                {
+                    if (queue.Count != 0 &&
+                    queue.Peek() % 2 == 0)
+                    {
+                        if (queue.Peek() % 4 == 0)
+                        {
+                            priorityQueue.Enqueue(queue.Count);
+                            queue.Dequeue();
+                        }
+                        else
+                        {
+                            ordinaryQueue.Enqueue(queue.Count);
+                            queue.Dequeue();
+                        }
+                    }
+                }
+
+                if (priorityQueue.Count != 0)
+                {
+                    dataGridViewProcessor2.Rows[0].Cells[amount2].Value = priorityQueue.Peek();
+                    dataGridViewProcessor2.Rows[0].Cells[amount2].Style.BackColor = Color.Yellow;
+                    priorityQueue.Dequeue();
+                    amount2++;     
+                    continue;
+                }
+
+                if (ordinaryQueue.Count != 0)
+                {
+                    dataGridViewProcessor2.Rows[0].Cells[amount2].Value = ordinaryQueue.Peek();
+                    dataGridViewProcessor2.Rows[0].Cells[amount2].Style.BackColor = Color.Brown;
+                    ordinaryQueue.Dequeue();
+                    amount2++;
+                    continue;
+                }
+
+                if (queue.Count == 0 &&
+                    customers == 100)
+                {
+                    //labelProcessor2.Text = "Processor2: " + Convert.ToString(amount);
+                    break;
+                }                   
+            }
+        }
+
+        private void updater_Tick(object sender, EventArgs e)
+        {
+            labelProcessor1.Text = "Processor1: " + Convert.ToString(amount1);
+            labelProcessor2.Text = "Processor1: " + Convert.ToString(amount2);
+            labelDenied.Text = "Denied: " + Convert.ToString(denies);
+        }
+    }
+}
